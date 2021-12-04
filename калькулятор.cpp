d1db9809e3846d0002f1531ef66070bea222278b
pol0praktika <95435519+pol0praktika@users.noreply.github.com>
2021-12-04 15:56:56 +0300

    Add files via upload

---
+++ калькулятор.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a, b;
+	char action;
+
+	cout << "введите 1 и 2 число" << endl;
+	cin >> a >> b;
+
+	cout << "введите действие" << endl;
+	cin >> action;
+
+	switch (action)
+	{
+	case '+':
+	{
+		cout << "результат:" << a + b << endl;
+
+		break;
+	}
+	case '-':
+	{
+		cout << "результат:" << a - b << endl;
+
+		break;
+	}
+	case '*':
+	{
+		cout << "результат:" << a * b << endl;
+
+		break;
+	}
+	case '/':
+	{
+		if (b == 0)
+		{
+			cout << "НЕТ РЕШЕНИЙ" << endl;
+			return 0;
+		}
+		cout << "результат:" << a/b << endl;
+
+
+		break;
+	}
+	default:
+		cout << "ДЕЙСТВИЕ НЕ ОПРЕДЕЛЕНО";
+		break;
+	}
+
+}
---
+++ поиск наиболее встречаемого слова.cpp
@@ -0,0 +1,49 @@
+﻿#include <string>
+#include <cstring>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+unordered_map<string, int> calc()
+{
+    unordered_map<string, int> words;
+    string k;
+
+
+    getline(cin, k);
+
+    char* c = &k[0];
+
+    char* dim = strtok(c, " ");
+
+    while (dim != 0)
+    {
+        words[string(dim)]++;
+        dim = strtok(0, " ");
+    }
+
+    return words;
+}
+
+int main()
+{
+    while (true)
+    {
+        cout << "Введите строку: " << endl;
+        unordered_map<string, int> words = calc();
+
+        int m = -1;
+        string m1 = "";
+        for (const auto& kv : words)
+        {
+            if (kv.second > m) {
+                m = kv.second;
+                m1 = kv.first;
+            }
+        }
+
+        cout <<"Часто встречающееся слово в строке: " << m1 << endl;
+    }
+    return 0;
+}
